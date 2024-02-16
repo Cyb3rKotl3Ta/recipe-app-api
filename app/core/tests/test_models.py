423cbf9b9232d2b8e80c899594bce2b93066a959
Oleksii kozulia <mistosisi@gmail.com>
2024-02-16 19:04:25 +0200

    create user&superuser models and test for it

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,32 @@ class ModelTests(TestCase):
         ]
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test123",
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        # self.assertTrue(user.is_active)
+        # self.assertEqual(user.email, "test@example.com")
+        # self.assertTrue(user.check_password("test123"))
+        # self.assertEqual(user.username, "test@example.com")
+        # self.assertEqual(user.first_name, "")
+        # self.assertEqual(user.last_name, "")
+        # self.assertEqual(user.date_joined, user.last_login)
+        # self.assertEqual(user.is_staff, True)
+        # self.assertEqual(user.is_active, True)
+        # self.assertEqual(user.is_superuser, True)
+        # self.assertEqual(user.groups.count(), 0)
+        # self.assertEqual(user.user_permissions.count(), 0)
+        # self.assertEqual(user.email, "test@example.com")
