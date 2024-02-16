30e6541aa7d02f21708af5f0fd7f822e031e38db
Oleksii kozulia <mistosisi@gmail.com>
2024-02-16 20:10:30 +0200

    a

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,12 +14,12 @@ class AdminSiteTests(TestCase):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
-            password='XXXXXXXXXXX',
+            password='testpass123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
-            password='XXXXXXXXXXX',
+            password='testpass123',
             name='Test User'
         )
 
